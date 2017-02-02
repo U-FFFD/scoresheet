f841f3287f73914a1c013f60f0d6eda33b76420e
Kerstin <kerstin.labuda@gmail.com>
2017-02-02 08:13:33 -0600

    Create TestSuite.java

---
+++ TestSuite.java
@@ -0,0 +1,11 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ 
+	scoreTest1.class, 
+	scoreTest2.class,
+	scoreTest3.class })
+
+public class TestSuite {
+}
