cedce24b470e8611f992459acd3dc930b54d1665
Owen Monsma <darkfire613@me.com>
2017-02-03 14:19:06 -0600

    fixed an extra space

--- TestSuite.java
+++ TestSuite.java
@@ -2,8 +2,8 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ 
-    ScoreTest1.class,
+@Suite.SuiteClasses({
+  ScoreTest1.class,
 	ScoreTest2.class,
 	ScoreTest3.class })
 
